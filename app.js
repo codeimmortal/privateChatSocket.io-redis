bf16e4e8288ab13ac6321f9d6bf16537f7c70615
himanshusharma <himanshu.sharma@oodlestechnologies.com>
2016-11-17 01:14:50 +0530

    added sub.on out of io scope

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var redis = require('redis');
 app.use(express.static('public'));
 
 server.listen(port, function () {
-    console.log("Server listening on: http://localhost:%s", port);
+    //console.log("Server listening on: http://localhost:%s", port);
 });
 
 app.get('/', function (req, res) {
@@ -20,77 +20,95 @@ var sub = redis.createClient();
 var pub = redis.createClient();
 var usernames = {};
 var rooms = [];
+var Sockets = {};
+var outerSocket ={};
 sub.subscribe("adduser");
 sub.subscribe("createroom");
 sub.subscribe("sendchat");
-//sub.subscribe("disconnect");
-io.sockets.on('connection', function (socket) {
-
-    sub.on('message',function(channel , message){
-        console.log("application-->",channel,message);
-   //     console.log(message);
+  sub.on('message',function(channel , message){
+        //console.log("application-->",channel,message,rooms);
+   //     //console.log(message);
          var jData= JSON.parse(message);
         if(channel == 'createroom'){
-          
+
            rooms.push(jData.room);
         } else if(channel == 'adduser'){
 
         var username = jData.username;
         var room = jData.room;
-        if (rooms.indexOf(jData) != -1) {
-            socket.username = username;
-            socket.room = room;
+        if (rooms.indexOf(room) != -1) {
+            Sockets[room].username = username;
+            Sockets[room].room = room;
             usernames[username] = username;
-            socket.join(room);
-            socket.emit('updatechat', 'SERVER', 'You are connected. Start chatting');
-            socket.broadcast.to(room).emit('updatechat', 'SERVER', username + ' has connected to this room');
-        } else {
-            socket.emit('updatechat', 'SERVER', 'Please enter valid code.'+rooms);
-        }
+            Sockets[room].socket.join(room);
+            }
         } else if(channel == 'sendchat'){
+            console.log(jData);
+            console.log(Sockets[jData.room])
+      //      console.log(Sockets);
 
-                    io.sockets.in(socket.room).emit('updatechat', socket.username, jData);
+                    io.sockets.in(  Sockets[jData.room].room).emit('updatechat', Sockets[jData.room].username, jData.data);
         }
-        console.log("rooms in app1 ",rooms);
-                    socket.emit(channel,message);
+        //console.log("rooms in app1 ",rooms);
+         //           socket.emit(channel,message);
          //   socket.broadcast.to(channel).emit("updatechat","server ping by pubsub");
 
     });
+//sub.subscribe("disconnect");
+io.sockets.on('connection', function (socket) {
+
+//outerSocket =socket;
+
+
+
 
      socket.on('createroom', function (data) {
         var new_room = ("" + Math.random()).substring(2, 7);
         rooms.push(new_room);
         data.room = new_room;
-             console.log("appp",rooms);
-        socket.emit('updatechat', 'SERVER', 'Your room is ready, invite someone using this ID:' + new_room);
-        socket.emit('roomcreated', data);
+
+             //console.log("appp",rooms);
+             if(Sockets[data.room]){
+
+             }else{
+               Sockets[data.room] = {};
+             }
+                Sockets[data.room].socket = socket;
+         Sockets[data.room].socket.emit('updatechat', 'SERVERon', new_room);
+         Sockets[data.room].socket.emit('roomcreated', data);
         pub.publish('createroom',JSON.stringify(data));
     });
 
     socket.on('adduser', function (data) {
-         console.log("applic1-->",room);
+         //console.log("applic1-->",room);
         var username = data.username;
         var room = data.room;
-        console.log("room",room);
-        
+        //console.log("room",Sockets);
+        if(Sockets[data.room]){
+
+        }else{
+          Sockets[data.room] = {};
+        }
+        Sockets[room].socket = socket;
         pub.publish('adduser',JSON.stringify(data));
         if (rooms.indexOf(room) != -1) {
-            socket.username = username;
-            socket.room = room;
-            usernames[username] = username;
-            socket.join(room);
-            socket.emit('updatechat', 'SERVER', 'You are connected. Start chatting');
-            socket.broadcast.to(room).emit('updatechat', 'SERVER', username + ' has connected to this room');
+          Sockets[room].username = username;
+           Sockets[room].room = room;
+           usernames[username] = username;
+           Sockets[room].socket.join(room);
+           Sockets[room].socket.emit('updatechat', 'SERVER', 'You are connected. Start chatting');
+           Sockets[room].socket.broadcast.to(room).emit('updatechat', 'SERVERon', room);
         } else {
-            socket.emit('updatechat', 'SERVER', 'Please enter valid code.'+rooms);
+            Sockets[room].socket.emit('updatechat', 'SERVER', 'Please enter valid code.'+rooms);
         }
     });
 
-   
+
 
     socket.on('sendchat', function (data) {
+//console.log
          pub.publish('sendchat',JSON.stringify(data));
-        io.sockets.in(socket.room).emit('updatechat', socket.username, data);
+      //  io.sockets.in(socket.room).emit('updatechat', socket.username, data);
     });
 
     // socket.on('disconnect', function () {
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,13 +12,19 @@ app.filter('reverse', function() {
 app.controller('AppCtrl', function ($scope, socket) {
 
   $scope.users = [];
+  $scope.roomsx=0;
   $scope.curtrentUser = '';
   socket.on('connect', function () { });
+  var roomId =0;
 
   socket.on('updatechat', function (username, data) {
     var user = {};
     user.username = username;
     user.message = data;
+    if (user.username == 'SERVERon'){
+  $scope.roomsx = user.message;
+console.log( "dsds", $scope.roomsx);
+    }
     user.date = new Date().getTime();
     user.image = 'http://dummyimage.com/250x250/000/fff&text=' + username.charAt(0).toUpperCase();
     $scope.users.push(user);
@@ -35,11 +41,17 @@ app.controller('AppCtrl', function ($scope, socket) {
 
   $scope.joinRoom = function (data) {
     $scope.curtrentUser = data.username;
+      $scope.roomsx = data.room;
+      console.log( "sdsd", $scope.roomsx);
     socket.emit('adduser', data);
   }
 
-  $scope.doPost = function (message) {
-    socket.emit('sendchat', message);
+  $scope.doPost = function (message1) {
+    console.log($scope.roomsx);
+ var message={};
+     message.room=$scope.roomsx
+     message.data=message1
+     socket.emit('sendchat', message);
   }
 });
 
